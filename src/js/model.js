6bc5926a28a25bf27888c97190e4055d5304e48a
kiethohoai <kietho.060193@gmail.com>
2024-11-02 09:26:39 +0700

    009 Refactoring for MVC

---
+++ src/js/model.js
@@ -0,0 +1,25 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  const res = await fetch(
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`,
+  );
+  if (!res.ok) throw new Error(`${res.statusText} ${res.status}`);
+  const data = await res.json();
+
+  let { recipe } = data.data;
+
+  state.recipe = {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+  };
+  console.log(`🚀  recipe =>`, recipe);
+};
